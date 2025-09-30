68c4434a198110edeeed39264f9cda96c0c68029
JimKarpodinis <dkarpodinis2@gmail.com>
2025-09-30 16:35:18 +0100

    feat: Include robot direction attribute

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 def main():
+    grid_size: int = 10
 
-	grid_size: int = 10
+    robots_name: str = (input("Please provide the robots name: "))
+    robots_id: int = int(input("please provide the robots ID: "))
+    robots_row_coord: int = int(input("Please provide the robots row coordinate: "))
+    robots_col_coord: int = int(input("Please provide the robots col coordinate: "))
 
-	robots_name: str = (input("Please provide the robots name: "))
-	robots_id: int = int(input("please provide the robots ID: "))
-	robots_row_coord: int = int(input("Please provide the robots row coordinate: "))
-	robots_col_coord: int = int(input("Please provide the robots col coordinate: "))
+    robots_direction: str = (input(
+        "Please provide the robots direction using one of 4 letters: 'w', 'e', 's', 'n': "
+        ))
 
-	robots_row_coord, robots_col_coord = normalise_coords(robots_row_coord, robots_col_coord, grid_size)
-	robots_coord_quadrant = get_robots_coord_quadrant(robots_row_coord, robots_col_coord, grid_size)
+    letter_to_direction = {'w': 'west',
+                           'e': 'east',
+                           'n': 'north',
+                           's': 'south'}
 
-	message = get_message(robots_name, robots_id, robots_row_coord, robots_col_coord, robots_coord_quadrant)
+    robots_direction = letter_to_direction[robots_direction]
 
-	print(message)
+    robots_row_coord, robots_col_coord = normalise_coords(robots_row_coord, robots_col_coord, grid_size)
+    robots_coord_quadrant = get_robots_coord_quadrant(robots_row_coord, robots_col_coord, grid_size)
+
+    message = get_message(robots_name, robots_id, robots_row_coord,
+                          robots_col_coord, robots_coord_quadrant, robots_direction)
+
+    print(message)
 
 
 def normalise_coords(robots_row_coord: int, robots_col_coord: int, grid_size: int):
@@ -53,13 +64,15 @@ def get_robots_coord_quadrant(robots_row_coord: int, robots_col_coord: int, grid
 	return robots_coord_quadrant
 
 
-def get_message(name: str, robots_id: int,  robots_row_coord: int, robots_col_coord: int, robots_coord_quadrant):
+def get_message(name: str, robots_id: int,  robots_row_coord: int, robots_col_coord: int, robots_coord_quadrant, robots_direction: str):
 
-	message = f"\n Robot's name is {name}. Robot's Id is: {robots_id}\n\n"
-	message += f"Robot's coordinates are: \n robots row coordinate: {robots_row_coord} robots col coordinate: {robots_col_coord} \n\n"
-	message += f"Robot's coordinate quadrant is: {robots_coord_quadrant}"
+    message = f"\n Robot's name is {name}. Robot's Id is: {robots_id}\n\n"
+    message += f"Robot's coordinates are: \n Robots row coordinate: {robots_row_coord}. Robots col coordinate: {robots_col_coord} \n\n"
+    message += f"Robot's coordinate quadrant is: {robots_coord_quadrant} \n\n"
 
-	return message
+    message += f"Robot is facing {robots_direction}"
+
+    return message
 
 
 if __name__ == "__main__":
