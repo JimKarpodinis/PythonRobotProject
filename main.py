ad4a3abb77498bd1514dc76ddd223b7a9dc8bb12
JimKarpodinis <dkarpodinis2@gmail.com>
2025-09-30 14:28:37 +0100

    feat: Add main script

---
+++ main.py
@@ -0,0 +1,18 @@
+def main():
+
+    grid_size: int = 10
+    age = 10
+    message = f"{name}is {age}years old."
+    print(message)
+
+    robots_name: str = (input("Please provide the robots name"))
+    robots_row_coord: int(input("Please provide the robots row coordinate"))
+    robots_col_coord: int = int(input("Please provide the robots col coordinate"))
+
+    # assert robots_col_coord > 0, "Robots coords should be positive integers"
+    # assert robots_row_coord > 0, "Robots coords should be positive integers"
+
+
+
+
+
