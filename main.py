a3737e722db9ddbd383f6ab394f18212b8ad04fa
JimKarpodinis <dkarpodinis2@gmail.com>
2025-10-02 15:28:03 +0100

    feat: Implement robot move, turn and find goal

--- main.py
+++ main.py
@@ -1,38 +1,84 @@
 import random
 
 def main():
-	grid_size: int = 10
 
-	robots_name: str = (input("Please provide the robots name: "))
-	robots_id: int = int(input("please provide the robots ID: "))
+    final_cell = (9, 9)
+    grid_size: int = 10
 
-	row_coord: int = random.randint(0, grid_size)
-	col_coord: int = random.randint(0, grid_size)
+    robots_name: str = (input("Please provide the robots name: "))
+    robots_id: int = int(input("please provide the robots ID: "))
 
-	direction: str = random.choice(["west", "east", "north", "south"])
+    row_coord: int = random.randint(0, grid_size)
+    col_coord: int = random.randint(0, grid_size)
 
-	row_coord, col_coord = normalise_coords(row_coord, col_coord, grid_size)
-	coord_quadrant = get_robots_coord_quadrant(row_coord, col_coord, grid_size)
+    direction: str = random.choice(["west", "east", "north", "south"])
 
-	print_init_message(robots_name, robots_id, row_coord,
-			  col_coord, coord_quadrant, direction)
+    row_coord, col_coord = normalise_coords(row_coord, col_coord, grid_size)
+    coord_quadrant = get_robots_coord_quadrant(row_coord, col_coord, grid_size)
+
+    print_init_message(robots_name, robots_id, row_coord,
+              col_coord, coord_quadrant, direction)
+
+    move_to_final_cell(row_coord, col_coord,
+                       direction, grid_size, final_cell)
 	
-	update_robot_position(row_coord, col_coord, grid_size, direction)
 	
-	
+def move_to_final_cell(row_coord: int, col_coord: int,
+                      direction: str, grid_size: int, final_cell: tuple):
+
+    coords = (row_coord, col_coord)
+
+    while coords != final_cell:
+
+        row_coord, col_coord = update_robot_position(
+                row_coord, col_coord, grid_size, direction)
+
+        direction = update_robot_direction(row_coord, col_coord, direction)
+
+        coords = (row_coord, col_coord)
+
+    print_final_cell_message()
+
+
+def print_final_cell_message():
+
+    print("I am drinking Rebena. I am happy !")
+
+
+def update_robot_direction(row_coord: int, col_coord: int, direction: str):
+
+    print_rotation_message()
+    direction = rotate_robot(direction)
+    print_position_message(row_coord, col_coord, direction)
+
+    return direction
+
+
+def print_rotation_message():
+
+    print("Turning 90 degrees clockwise \n")
+
 
 def update_robot_position(row_coord: int, col_coord: int, grid_size: int, direction: str):
 	
-	while not is_border(row_coord, col_coord, grid_size):
+    while can_move(row_coord, col_coord, grid_size, direction):
 
-		print_update_message(False)
-		row_coord, col_coord = move_robot(row_coord, col_coord, direction) 
-		print_position_message(row_coord, col_coord, direction)
+        print_update_message(True)
+        row_coord, col_coord = move_robot(row_coord, col_coord, direction) 
+        print_position_message(row_coord, col_coord, direction)
 
-	
-	print_update_message(True)
-		
+    print_update_message(False)
 
+    return row_coord, col_coord
+
+
+def can_move(row_coord: int, col_coord: int, grid_size: int, direction: str) -> bool:
+
+        row_coord, col_coord = move_robot(row_coord, col_coord, direction)
+
+        can_move = not is_over_border(row_coord, col_coord, grid_size)
+
+        return can_move
 
 def normalise_coords(row_coord: int, col_coord: int, grid_size: int):
 		
@@ -73,26 +119,37 @@ def get_robots_coord_quadrant(row_coord: int, col_coord: int, grid_size: int):
 
 def move_robot(row_coord: int, col_coord: int, direction: str) -> tuple:
 	
+
 	move_direction = {"east": (0,-1), "west": (0,1), "north": (-1,0), "south": (1,0)}
 
-	
 	current_position =  (row_coord, col_coord)
 
 	updated_position = tuple(map(sum, zip(
 		current_position, move_direction[direction])))
 	
-	breakpoint()
 	return updated_position[0], updated_position[1]
 
 
-def is_border(row_coord: int, col_coord: int, grid_size) -> bool:
+def rotate_robot(direction: str) -> str:
+
+    new_direction = {"north": "east",
+                     "south": "west",
+                     "west": "north",
+                     "east": "south"}
+
+    return new_direction[direction]
+
+
+
+def is_over_border(row_coord: int, col_coord: int, grid_size) -> bool:
 	
-	is_down_border = (row_coord == (grid_size -1) or (col_coord == (grid_size -1)))
+	is_over_down_border = (row_coord > (grid_size -1) or (col_coord > (grid_size -1)))
 
-	is_upper_border = ((row_coord == 0) or (col_coord == 0)) 
+	is_over_upper_border = ((row_coord < 0) or (col_coord < 0)) 
 
-	is_border = is_upper_border or is_down_border
-	return is_border
+	is_over_border = is_over_upper_border or is_over_down_border
+
+	return is_over_border
 		
 	
 def print_position_message(row_coord: int, col_coord: int, direction: str):
@@ -100,9 +157,9 @@ def print_position_message(row_coord: int, col_coord: int, direction: str):
 	print(f"I am curently at {(row_coord,col_coord)}, facing {direction}")
 
 
-def print_update_message(is_border: bool):
+def print_update_message(can_move: bool):
 	
-	if not is_border:
+	if can_move:
 		print("Moving one step forward.")
 	else:
 		print("I have a wall in front of me !")
